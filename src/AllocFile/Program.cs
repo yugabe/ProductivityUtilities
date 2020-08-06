6556f2c37e3f0be4a6b176715f378b9bb53fad9d
Szabó Gábor <szabo.gabor@aut.bme.hu>
2020-08-06 16:15:42 +0200

    AllocFile v1

---
+++ src/AllocFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AllocFile
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if ((args?.Length) >= 2 && int.TryParse(args[1], out var size))
+            {
+                var file = new FileInfo(args[0]);
+                if (file.Exists)
+                {
+                    Console.WriteLine("The file already exists.");
+                    return 2;
+                }
+                using var fs = file.Create();
+                fs.Write(new byte[size]);
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("Usage:   allocfile filename filelength\nExample: allocfile myfile.dat 10000");
+                return 1;
+            }
+        }
+    }
+}
